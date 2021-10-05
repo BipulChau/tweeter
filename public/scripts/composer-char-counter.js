184a797648cfdb2ef0cd38f8c59bbdf565b80a1f
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-05 17:21:08 -0400

    It will change the color of counter to red when negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,8 +4,18 @@ $('#tweet-text').on("input", function(event){
     //console.log($(this).val().length);
     let length = $(this).val().length;
     //console.log($(this).parent().children(".justTwitIt"))
-    $(this).parent().children(".justTwitIt").children(".counter").text(140-length)
+    let max =140;
+    $(this).parent().children(".justTwitIt").children(".counter").text(140-length) ;
+    if(length > max) {
+      $(this).parent().children(".justTwitIt").children(".counter").css({"color": "red"})
+      
+    }
+// $(this).parent().children(".justTwitIt").children(".counter").text(140-length) 
 
+
+    
+    //   $(this).parent().children(".justTwitIt").children(".counter").css({"color": "red"})
+    // }
 })
 
 
