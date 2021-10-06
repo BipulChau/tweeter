5739a227904528fd994daeffcf77c850b0bde733
Bipul Chaudhary <bipulchau@gmail.com>
2021-10-06 18:50:47 -0400

    Form submission using jquery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,4 +1,3 @@
-
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
@@ -7,56 +6,64 @@
 
 const data = [
   {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    "created_at": 1461116232227
+    created_at: 1461116232227,
   },
   {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
     },
-    "created_at": 1461113959088
-  }
-]
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
 
 $(() => {
-  $.get("/tweets/", data, (data) =>{
-    console.log("Data: ", data)
-    const singleTweet = data[0];
-    //const $tweet = createTweetElement(singleTweet);
-    //console.log($tweet)
-    //const $tweetContainer = $(".tweetContainer");
-    //$tweetContainer.append($tweet);
-    renderTweets(data);
+  // making a get request to see some data... jQuery.get( url [, data ] [, success ] [, dataType ] )
+  const loadTweets = () => {
+    $.get(
+      "/tweets/",
+      data,
+      (data) => {
+        //console.log("Data: ", data);
+        //const singleTweet = data[0];
+        //const $tweet = createTweetElement(singleTweet);
+        //console.log($tweet)
+        //const $tweetContainer = $(".tweetContainer");
+        //$tweetContainer.append($tweet);
+        renderTweets(data);
+      },
+      "json"
+    );
+  };
 
-  }, "json")
+  loadTweets();
 
-  
-  
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
     const $tweetContainer = $(".tweetContainer");
-    $tweetContainer.empty();
+    $tweetContainer.empty(); // clear out blog-container
+    // repopulate blog-container
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $tweetContainer.append($tweet);
+      $tweetContainer.prepend($tweet);
     }
-  }
-  
-  const createTweetElement = function(tweet) {
+  };
+
+  const createTweetElement = function (tweet) {
     // let $tweet = /* Your code for creating the tweet element */
     // ...
     let $tweet = $(`
@@ -89,11 +96,22 @@ $(() => {
              </footer>
   
         </article>
-    `)
+    `);
 
     return $tweet;
-  }
+  };
   //return $tweet;
   //renderTweets(data);
-})
 
+  const $form = $("#new-tweet-form");
+  $form.on("submit", function (event) {
+    event.preventDefault();
+    //console.log("form was submitted");
+    const serializedData = $(this).serialize();
+    //console.log(serializedData);
+    $.post("/tweets/", serializedData, (response) => {
+     // console.log(response);
+      loadTweets();
+    });
+  });
+});
